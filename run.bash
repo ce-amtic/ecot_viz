34c9d183138ddf5fcd599f7b96d4053b183a8a50
ce-amtic <fyh516@126.com>
2025-11-20 12:22:45 +0000

    feat: update run.bash to use new dataset path and change save directory

--- run.bash
+++ run.bash
@@ -1,9 +1,8 @@
 python viz_video.py --dataset_paths \
-    /pretrain_data/oxe_lerobot/berkeley_autolab_ur5 \
-    /pretrain_data/oxe_lerobot/utaustin_mutex \
+    /pretrain_data/oxe_lerobot/nyu_door_opening_surprising_effectiveness \
     --bbox_jsonl_path /pretrain_data/annotation/v2/bbox/ \
     --sub_task_jsonl_path /pretrain_data/annotation/v2/sub_task/ \
-    --save_dir tmp/ \
+    --save_dir /pretrain_data/bbox_videos/for_review/ \
     --font_path Ubuntu-Regular.ttf \
     --show_object_name
     # --show_plans
